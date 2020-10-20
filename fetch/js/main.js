cde95c9f3029e738b87d2d1d556bb5bc43fd9a60
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-10-20 23:05:19 +0900

    learn to fetch and async/await

---
+++ fetch/js/main.js
@@ -0,0 +1,27 @@
+const url = 'https://dog.ceo/api/breeds/image/random';
+const options = {
+  method: 'GET'
+};
+
+const fetchTest1 = fetch(url, options);
+console.log(fetchTest1);
+
+// const fetchTest2 = fetch(url, options)
+//   .then(response => response.json());
+
+// console.log(fetchTest2);
+
+function getDogImage(url, options){
+  return fetch(url, options)
+    .then( response => response.json());
+}
+
+async function getImage(url, options){
+  const response = await getDogImage(url, options);
+  // console.log(response.message);
+  const imageElement = document.createElement('img');
+  imageElement.src = response.message;
+  document.getElementById('image').appendChild(imageElement);
+}
+
+getImage(url, options);

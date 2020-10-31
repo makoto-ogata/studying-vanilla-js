c78e808d4c8c4ec198c7a7f535deb4b9f3b36e61
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-10-31 23:17:40 +0900

    todo app section 4

--- ToDo/src/model/TodoListModel.js
+++ ToDo/src/model/TodoListModel.js
@@ -46,9 +46,34 @@ export class TodoListModel extends EventEmitter {
    * @param {TodoItemModel} todoItem
    */
 
-   addTodo(todoItem){
-     this.items.push(todoItem);
+  addTodo(todoItem){
+    this.items.push(todoItem);
+    this.emitChange();
+  }
+
+  /**
+   * 指定したidのTodoItemのcompletedを更新する
+   * @param {{ id:number, completed: boolean }}
+   */
+
+  updateTodo({ id, completed }) {
+    const todoItem = this.items.find(todo => todo.id === id);
+    if(!todoItem){
+      return;
+    }
+    todoItem.completed = completed;
+    this.emitChange();
+  }
+
+  /**
+   * 指定したidのTodoItemを削除する
+   * @param {{ id: number }}
+   */
+
+   deleteTodo({id}){
+     this.items = this.items.filter(todo => {
+       return todo.id !== id;
+     });
      this.emitChange();
    }
-
 }
